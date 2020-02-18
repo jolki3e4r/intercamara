8c340907d0f0c4c50af914489112633231ee4d67
jolki3e4r <jolki3e4r@hotmail.com>
2020-02-18 13:51:55 -0500

    creando los end points

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 
 class inicio extends Component {
 
-    onClick = (event) =>{
+    onSubmit = (event) =>{
         event.preventDefault()
         
+        let ipCamenra= event.target.elements[0].value
+        let password = event.target.elements[1].value
+        console.log('daos capturados', ipCamenra , password)
+        
+        let url= `http://${ipCamera}:8090/getDeviceKey`
+
+        fetch(url, {
+        method: 'POST', 
+        headers:{
+            'Content-Type': 'application/json'
+        }
+        }).then(res => res.json())
+        .catch(error => console.error('Error:', error))
+        .then(response => console.log('Success:', response));
+
     }
+
+
+
 
     render() { 
         return (  
             <>
             <h1>Esta es la pagina de inicio </h1>
-            <form className="form-inline">
+            <form className="form-inline" onSubmit={ this.onSubmit} >
                 <div class="form-group mb-2">
                     <label htmlFor="staticEmail2" className="sr-only">Ip de la camara</label>
                     <input type="text"
-                        readonly classNames="form-control-plaintext"
+                        readonly className="form-control-plaintext"
                         id="ipCamera" 
-                        value="email@example.com" />
+                        placeholder="ip de camara"/>
                 </div>
                 <div className="form-group mx-sm-3 mb-2">
                     <label htmlFor="inputPassword2" className="sr-only">Password</label>
@@ -26,7 +44,8 @@ class inicio extends Component {
                           id="inputPassword2"
                           placeholder="Password"/>
                 </div>
-                <button type="submit" className="btn btn-primary mb-2" onClick={onclick}>Confirmar </button>
+                <button type="submit"
+                        className="btn btn-primary mb-2">Confirmar </button>
             </form>
             </>
         );
